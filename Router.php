1526334c2a7b06a85ccc11ef1a6cf5b303c3b395
Cintia <cintiaa_utrera@outlook.es>
2023-09-08 12:59:40 -0300

    registrando URL's y los metodos

--- Router.php
+++ Router.php
@@ -3,8 +3,30 @@
 namespace MVC;
 
 class Router{
-    public function  __construct()
-    {
-        echo "Creando el Router..";
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn) {
+        $this->rutasGET[$url] = $fn;
+        
     }
+
+    public function  comprobarRutas(){
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if($metodo === 'GET'){
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if($fn){
+            // la url existe y hay una funcion asociada
+            call_user_func($fn, $this); // llama a una funcion cuando no se sabe el nombre
+        } else {
+
+        }
+    }
+
+
 }
--- public/index.php
+++ public/index.php
@@ -5,3 +5,5 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 
 $router = new Router();
+
+$router->comprobarRutas(); //valida el tipo de request sea get o post
