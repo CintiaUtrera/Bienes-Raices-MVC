679f1e62b31579f4de5eb9902b5141277ba12217
Cintia <cintiaa_utrera@outlook.es>
2023-09-11 12:40:30 -0300

    asociando URL a metodos del  controlador

---
+++ controllers/PropiedadController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+class PropiedadController{
+    public static function index(){
+
+    }
+
+    public static function crear() {
+        
+    }
+
+    public static function actualizar() {
+        
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,7 +3,12 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\PropiedadController;
 
 $router = new Router();
+
+$router->get('/admin', [PropiedadController::class, 'index']);
+$router->get('/propiedades/crear', [PropiedadController::class, 'crear']);
+$router->get('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 
 $router->comprobarRutas(); //valida el tipo de request sea get o post
