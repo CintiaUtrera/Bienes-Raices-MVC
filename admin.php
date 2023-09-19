70f27afdefd70a3f0287c05cd585895d449030a2
Cintia <cintiaa_utrera@outlook.es>
2023-09-19 11:17:00 -0300

    pasar resultados hacia la vista

---
+++ admin.php
@@ -0,0 +1,49 @@
+<main class="contenedor seccion">
+        <h1>Administrador de Bienes Raices</h1>
+
+        <?php if($resultado) {
+                $mensaje = mostrarNotificacion(intval($resultado));
+                if($mensaje) { ?>
+                <p class="alerta exito"><?php echo s($mensaje) ?> </p>
+            <?php } 
+        } 
+        ?>
+
+        <a href="../admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+        <a href="../admin/vendedores/crear.php" class="boton boton-amarillo">Nuevo Vendedor</a>
+
+        <h2>Propiedades</h2>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody> <!-- Mostrar los resultados --> 
+            <?php foreach($propiedades as $propiedad): ?>
+                <tr>
+                    <td><?php echo $propiedad->id; ?></td>
+                    <td><?php echo $propiedad->titulo; ?></td>
+                    <td class="imagen-tabla"> <img src="/bienesraices/imagenes/<?php echo $propiedad->imagen; ?>" > </td>
+                    <td>$ <?php echo $propiedad->precio; ?></td>
+                    <td>
+                        <form method="POST">
+                            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>" >
+                            <input type="hidden" name="tipo" value="propiedad" >
+                            <input type="submit" class="boton-rojo-block" value="Eliminar" >
+                        </form>
+                        
+                        <a href="../admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                    
+                    </td>
+                </tr>
+
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+</main>
--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -2,11 +2,17 @@
 
 namespace Controllers;
 use MVC\Router;
+use Model\Propiedad;
 
 class PropiedadController{
     public static function index(Router $router){
+        $propiedades= Propiedad::all();
+        $resultado = null;
+
         $router->render('propiedades/admin', [
-            
+            'propiedades' => $propiedades,
+            'resultado' => $resultado
+
         ]);
     }
 
