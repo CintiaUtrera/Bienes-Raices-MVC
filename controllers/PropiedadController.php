66cf96170975c5cd2b07f287a167911771be7b22
Cintia <cintiaa_utrera@outlook.es>
2023-09-21 16:28:12 -0300

    almacenar imagenes en carpeta public

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -4,11 +4,14 @@ namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PropiedadController{
     public static function index(Router $router){
         $propiedades= Propiedad::all();
-        $resultado = null;
+        
+        //Muestra resultado
+        $resultado = $_GET['resultado'] ?? null;
 
         $router->render('propiedades/admin', [
             'propiedades' => $propiedades,
@@ -24,11 +27,43 @@ class PropiedadController{
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
             
+        //Crea una nueva instancia 
+        $propiedad  = new Propiedad($_POST['propiedad']);
+
+        //SUBIDA DE ARCHIVOS
+        
+        //Generar un nombre único
+        $nombreImagen = md5( uniqid( rand(), true) ) . ".jpg";
+
+        // SETEAR la imagen
+        // Realiza un resize a la imagen con intervention 
+        if($_FILES['propiedad']['tmp_name']['imagen']){
+            $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
+            $propiedad->setImagen($nombreImagen);
+        }
+        
+
+        //Validar
+        $errores = $propiedad->validar();
+
+        if(empty($errores)){
+            //Crear carpeta
+            $carpetaImagenes = '../../imagenes/';
+                if(!is_dir(CARPETA_IMAGENES)){
+                    mkdir(CARPETA_IMAGENES);
+                }
+            // guarda la imagen en el servidor 
+            $image->save(CARPETA_IMAGENES . $nombreImagen);
+            // Guarda en la base de datos 
+            $propiedad->guardar();
+
+            }
         }
 
         $router->render('propiedades/crear', [
             'propiedad' => $propiedad,
-            'vendedores' => $vendedores
+            'vendedores' => $vendedores,
+            'errores' => $errores
         ]);
     }
 
--- views/propiedades/crear.php
+++ views/propiedades/crear.php
@@ -1,6 +1,9 @@
 <main class="contenedor seccion">
     <h1>Crear</h1>
-    <form class="formulario" method="POST">
+    <?php foreach($errores as $error): ?>
+        <div class="alerta error"> <?php echo $error; ?> </div>
+    <?php endforeach; ?>
+    <form class="formulario" method="POST" enctype="multipart/form-data">
         <?php include __DIR__ . '/formulario.php'; ?>
 
         <input type="submit" value="Crear Propiedad" class="boton boton-verde">
