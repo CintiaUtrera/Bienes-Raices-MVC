76abd5b03c9891ba791aa2058e0d3435cc0302a1
Cintia <cintiaa_utrera@outlook.es>
2023-09-22 12:44:51 -0300

    creando controlador para vendedores

---
+++ controllers/VendedorController.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Controllers;
+
+class VendedorController{
+    public static function crear() {
+        
+    }
+    public static function actualizar() {
+        
+    }
+    public static function eliminar() {
+        
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\PropiedadController;
+use Controllers\VendedorController;
 
 $router = new Router();
 
@@ -13,5 +14,11 @@ $router->post('/propiedades/crear', [PropiedadController::class, 'crear']);
 $router->get('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 $router->post('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 $router->post('/propiedades/eliminar', [PropiedadController::class, 'eliminar']);
+
+$router->get('/vendedores/crear', [VendedorController::class, 'crear']);
+$router->post('/vendedores/crear', [VendedorController::class, 'crear']);
+$router->get('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
+$router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
+$router->post('/vendedores/eliminar', [VendedorController::class, 'eliminar']);
 
 $router->comprobarRutas(); //valida el tipo de request sea get o post
