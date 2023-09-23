4e60af740bbb9271dec34989a052071ce971ce9f
Cintia <cintiaa_utrera@outlook.es>
2023-09-23 10:53:28 -0300

    creando URL's

---
+++ controllers/PaginasController.php
@@ -0,0 +1,28 @@
+<?php
+namespace Controllers;
+
+class PaginasController{
+    public static function index(){
+
+}
+public static function nosotros(){
+
+}
+public static function propiedades(){
+
+}
+public static function propiedad(){
+
+}
+public static function blog(){
+
+}
+public static function entrada(){
+
+}
+public static function contacto(){
+
+}
+
+
+}
--- public/index.php
+++ public/index.php
@@ -3,11 +3,13 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
-use Controllers\PropiedadController;
+use Controllers\PaginasController;
 use Controllers\VendedorController;
+use Controllers\PropiedadController;
 
 $router = new Router();
 
+// Zona privada
 $router->get('/admin', [PropiedadController::class, 'index']);
 $router->get('/propiedades/crear', [PropiedadController::class, 'crear']);
 $router->post('/propiedades/crear', [PropiedadController::class, 'crear']);
@@ -20,6 +22,17 @@ $router->post('/vendedores/crear', [VendedorController::class, 'crear']);
 $router->get('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
 $router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
 $router->post('/vendedores/eliminar', [VendedorController::class, 'eliminar']);
+
+// Zona publica
+$router->get('/', [PaginasController::class, 'index']);
+$router->get('/nosotros', [PaginasController::class, 'nosotros']);
+$router->get('/propiedades', [PaginasController::class, 'propiedades']);
+$router->get('/propiedad', [PaginasController::class, 'propiedad']);
+$router->get('/blog', [PaginasController::class, 'blog']);
+$router->get('/entrada', [PaginasController::class, 'entrada']);
+$router->get('/contacto', [PaginasController::class, 'contacto']);
+$router->post('/contacto', [PaginasController::class, 'contacto']);
+
 
 $router->comprobarRutas(); //valida el tipo de request sea get o post
 
