505ba2aaaa72de731945d6dda99151ca0344c6ef
Cintia <cintiaa_utrera@outlook.es>
2023-09-25 12:25:51 -0300

    creando pagina blog

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -41,10 +41,12 @@ class PaginasController{
     }
 
     public static function blog(Router $router){
-
+        $router->render('paginas/blog', []);
     }
+
     public static function entrada(Router $router){
 
+        $router->render('paginas/entrada', []);
     }
     public static function contacto(Router $router){
 
