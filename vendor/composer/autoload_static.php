e1f7d67739dc6a7ae1dc6d0d3650090c11d25fec
Cintia <cintiaa_utrera@outlook.es>
2023-09-25 13:07:42 -0300

    creando pagina contacto

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -14,6 +14,7 @@ class ComposerStaticInit11e41460287a0533e711a3397ec29f14
         'P' => 
         array (
             'Psr\\Http\\Message\\' => 17,
+            'PHPMailer\\PHPMailer\\' => 20,
         ),
         'M' => 
         array (
@@ -39,6 +40,10 @@ class ComposerStaticInit11e41460287a0533e711a3397ec29f14
         array (
             0 => __DIR__ . '/..' . '/psr/http-factory/src',
             1 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Model\\' => 
         array (
