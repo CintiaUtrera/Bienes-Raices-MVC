5d05a45369de72c5495e9b6b723d774c589ee81f
Cintia <cintiaa_utrera@outlook.es>
2023-09-28 12:14:22 -0300

    validando el formulario de  autenticacion

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -8,10 +8,25 @@ class LoginController{
     public static function login(Router $router){
         $errores = [];
 
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $auth = new Admin($_POST);
+
+            $errores = $auth->validar();
+
+            if(empty($errores)){
+                //verificar si el usuario exite
+
+                //verificar el password
+
+                // autenticar el usuario
+            }
+        }
+
         $router->render('auth/login', [
             'errores' => $errores,
         ]);
     }
+
     public static function logout(Router $router){
         
     }
--- models/Admin.php
+++ models/Admin.php
@@ -3,5 +3,28 @@
 namespace Model;
 
 class Admin extends ActiveRecord{
-    
+    // Base de datos
+    protected static $tabla = 'usuarios';
+    protected static $columnaDB = ['id', 'email', 'password'];
+
+    public $id;
+    public $email;
+    public $password;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+    }
+
+    public function validar(){
+        if(!$this->email){
+            self::$errores[] = 'El Email es obligatorio';
+        }
+        if(!$this->password){
+            self::$errores[] = 'El Password es obligatorio';
+        }
+        return self::$errores;
+    }
 }
